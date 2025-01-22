34147a1f7807f772d23663297e51ebc0c422930d
andrei.habas <habasescuandrei1234@gmail.com>
2025-01-22 18:26:22 +0200

    Initialization

---
+++ src/test/java/com/jobhunter24/AuthenticationAPI/AuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobhunter24.AuthenticationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
